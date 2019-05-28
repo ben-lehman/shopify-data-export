d560d315a7984555006d2d18907000804f2f895b
Ben Lehman <ben.a.lehman@gmail.com>
2019-05-28 16:23:45 -0400

    Refactored app using Blueprints

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+from config import Config as cfg
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app=Flask(__name__, instance_relative_config=True)
+    app.debug = True
+    app.secret_key = cfg.SECRET_KEY
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load test config if passed in
+        app.config.from_mapping(test_config)
+
+    from . import connect
+    app.register_blueprint(connect.bp)
+
+    return app
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080)
